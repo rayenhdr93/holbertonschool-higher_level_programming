d3f99ba0deae706a962937618e74ad56e0c6361e
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-09 16:47:30 +0100

    1-

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- COMMENT
-CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
