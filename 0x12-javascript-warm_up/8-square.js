12f6a4c3e5225cf379c1a013a48750bb714c6159
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-04 16:25:04 +0000

    fastin and workin dosent match xDD

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    txt += '#';
+    txt += 'X';
   }
   for (let i = 0; i < process.argv[2]; i++) {
     console.log(txt);
