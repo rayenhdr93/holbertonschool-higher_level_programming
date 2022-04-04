d77a1e2b7f2978443aea4526bd538c1e6c30d240
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-04 16:23:57 +0000

    8-

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const process = require('process');
+let txt = '';
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    txt += '#';
+  }
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log(txt);
+  }
+}
