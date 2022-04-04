72b49b05ff37e6bcd93def37d88a9157e11e0650
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-04 18:35:12 +0100

    11-

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,16 +4,16 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   let s = 0;
-  let x = parseInt(process.argv[1]);
-  let y = parseInt(process.argv[2]);
+  let x = parseInt(process.argv[2]);
+  let y = parseInt(process.argv[3]);
   if (x < y) {
     s = x;
     x = y;
     y = s;
   }
   for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] < x && process.argv[i] > y) {
-      y = process.argv[i];
+    if ((parseInt(process.argv[i]) < x) && (parseInt(process.argv[i]) > y)) {
+      y = parseInt(process.argv[i]);
     }
   }
   console.log(y);
