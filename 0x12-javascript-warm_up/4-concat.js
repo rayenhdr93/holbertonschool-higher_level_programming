77563c4d09be77fe314daa475395301b40d2ff67
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-04 14:18:57 +0000

    4- fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,6 +2,8 @@
 const process = require('process');
 if (process.argv[2] && process.argv[3]) {
   console.log(process.argv[2], 'is', process.argv[3]);
+} else if (process.argv[2]) {
+  console.log(process.argv[2], 'is', 'undefined');
 } else {
-  console.log('No argument');
+  console.log('undefined', 'is', 'undefined');
 }
