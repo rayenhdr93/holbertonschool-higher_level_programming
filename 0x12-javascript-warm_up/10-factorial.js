88bd520d18db2a2dc224302de5fb410cdbffe6d6
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-04 18:04:41 +0100

    10-

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function fact (n) {
+  let x = n;
+  for (let i = n - 1; i > 0; i--) {
+    x = x * i;
+  }
+  console.log(x);
+}
+const process = require('process');
+if (isNaN(process.argv[2])) {
+  console.log('NaN');
+} else {
+  fact(process.argv[2]);
+}
