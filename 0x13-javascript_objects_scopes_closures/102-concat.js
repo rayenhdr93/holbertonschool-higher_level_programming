4e089513203b191d6780d034f59616a5e1606b0c
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-05 16:55:23 +0100

    rlly weird

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-const process = require('process');
 const fs = require('fs');
-let txt1 = '';
-if (process.argv.length === 5) {
-  fs.readFile(process.argv[2], (err, data) => {
-    if (err) throw err;
-
-    txt1 = data;
-  });
-  fs.readFile(process.argv[3], (err, data) => {
-    if (err) throw err;
-    txt1 = txt1 + '\n' + data + '\n';
-    fs.writeFile(process.argv[4], txt1, (err) => {
-      if (err) throw err;
-    });
-  });
-}
+const txt1 = fs.readFileSync(process.argv[2]);
+const txt2 = fs.readFileSync(process.argv[3]);
+fs.writeFile(process.argv[4], txt1 + '\n' + txt2 + '\n', function (err) {
+  if (err) throw err;
+});
