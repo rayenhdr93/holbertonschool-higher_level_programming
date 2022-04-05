32dac57b6b0a40799278b6da21684192168cdbb3
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-05 02:38:01 +0100

    6-

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      let txt = '';
+      for (let i = 0; i < this.width; i++) {
+        txt += 'C';
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(txt);
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
