66b65fa340498a37a91a65f352a2a54b1fa7ef6b
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-05 15:58:38 +0100

    100- fixed

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 const array1 = require('./100-data.js').list;
+const map1 = [];
 console.log(array1);
-const map1 = array1.map(x => x * (x - 1));
+for (let i = 0; i < array1.length; i++) {
+  if (i === 0) {
+    map1[i] = 0;
+  } else {
+    map1[i] = array1[i] * array1[i - 1];
+  }
+}
 console.log(map1);
