84d90ba07f9ec09e1b7d777b44eb8fe08d78a50c
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-05 02:25:29 +0100

    4-

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let txt = '';
+    for (let i = 0; i < this.width; i++) {
+      txt += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(txt);
+    }
+  }
+
+  rotate () {
+    const x = this.width;
+    this.width = this.height;
+    this.height = x;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
