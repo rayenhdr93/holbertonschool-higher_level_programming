421970307c685b09ab5dabc661190f9ff427bd2b
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-10 00:16:20 +0100

    2- fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
             argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
-        if row[1]:
+        if row[1] == argv[4]:
             print(row)
     cur.close()
     conn.close()
