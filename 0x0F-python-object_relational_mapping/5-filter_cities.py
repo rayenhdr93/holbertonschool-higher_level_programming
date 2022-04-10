ed88571e4c33c8e497fbea3cf9e0ee47bd3fe67c
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-10 01:10:15 +0100

    5-

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Required'''
+
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[
+        1], passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities, states \
+            WHERE states.name LIKE %s AND cities.state_id = states.id \
+                ORDER BY cities.id ASC", (argv[4],))
+    query_rows = cur.fetchall()
+    str = ""
+    for row in query_rows:
+        str = str + row[0] + " "
+    print(str)
+    cur.close()
+    conn.close()
