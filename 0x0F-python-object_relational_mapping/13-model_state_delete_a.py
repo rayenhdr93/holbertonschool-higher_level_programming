4e4b694e6dcc62425189c1be7464dcc83dde6c9b
rayenhdr93 <hedrirayen69@gmail.com>
2022-04-10 20:51:13 +0100

    13-

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
     session = Session(engine)
     for state in session.query(State).order_by(State.id).all():
         if ('a' in state.name):
-            obj = session.query(State).get(state.id)
-            session.delete(obj)
+            session.delete(state)
     session.commit()
     session.close()
