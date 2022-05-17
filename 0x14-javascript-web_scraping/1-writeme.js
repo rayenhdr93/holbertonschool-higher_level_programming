b7d4c405ff53ea6c21d879e8a4b07951ae869dae
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-17 18:46:44 +0100

    1- fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const myArgs = process.argv.slice(2);
-fs.writeFile(myArgs[2], myArgs[3], (err) => {
+const process = require('process');
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
   if (err) throw err;
 });
