22da64aacbbea76c32b583f3631af12f7098147e
rayenhdr93 <hedrirayen69@gmail.com>
2022-07-11 20:44:48 +0100

    8-

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''doc'''
+
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    args = {'q': ""}
+    if len(argv) > 1:
+        args['q'] = argv[1]
+    req = requests.post('http://0.0.0.0:5000/search_user', data=args)
+    je = {}
+    try:
+        je = req.json()
+    except Exception:
+        print('Not a valid JSON')
+    if len(je):
+            print('[{}] {}'.format(je['id'], je['name']))
+    else:
+        print('No result')
